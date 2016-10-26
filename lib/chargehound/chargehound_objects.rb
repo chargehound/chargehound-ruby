925554d25ce2f8cf4b618f0d9e3ad7b30fa133d4
jj <jj.weber@gmail.com>
2016-10-26 15:39:24 -0700

    Add missing file

---
+++ lib/chargehound/chargehound_objects.rb
@@ -0,0 +1,36 @@
+require 'ostruct'
+
+module Chargehound
+  # Base class for Chargehound models
+  class ChargehoundObject < OpenStruct
+    def to_json(*a)
+      as_json(*a).to_json
+    end
+
+    def as_json(*_a)
+      hash = {}
+      each_pair do |key, value|
+        hash[key] = convert(value)
+      end
+      hash
+    end
+
+    def convert(value)
+      if value.is_a?(OpenStruct)
+        value.as_json
+      elsif value.is_a?(Array)
+        value.map { |item| convert(item) }
+      elsif value.is_a?(Struct)
+        value.to_h
+      else
+        value
+      end
+    end
+  end
+
+  class Dispute < ChargehoundObject
+  end
+
+  class List < ChargehoundObject
+  end
+end
